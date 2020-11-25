08ec09de66018d1a680b5f0ac370f0e7c42fbe17
Aaron Klump <sourcecode@intheloftstudios.com>
2020-11-24 18:40:28 -0800

    Setup API for javascript testing.

--- example/tests/runner.php
+++ example/tests/runner.php
@@ -6,6 +6,6 @@
  */
 
 load_config('config/local');
-//run_suite('suite_dev_only');
-//run_suite('suite');
+run_suite('suite_dev_only');
+run_suite('suite');
 run_suite('javascript', ['js' => TRUE]);
