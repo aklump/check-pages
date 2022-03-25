667130aa4b97cfe4feb3a3fc30c4668061f1aae5
Aaron Klump <sourcecode@intheloftstudios.com>
2022-03-24 21:49:34 -0700

    Update the import plugin and fix a bug with it.

--- plugins/import/Import.php
+++ plugins/import/Import.php
@@ -12,15 +12,15 @@ use Symfony\Component\Yaml\Yaml;
  */
 final class Import extends LegacyPlugin {
 
+  // TODO Replace with \AKlump\CheckPages\Parts\Suite::replaceTestWithMultiple
   public function onLoadSuite(SuiteEventInterface $event) {
     $suite = $event->getSuite();
-    $new_data = $suite->jsonSerialize();
-    $needs_update = FALSE;
-    foreach ($suite->getTests() as $index => $test) {
+    foreach ($suite->getTests() as $test) {
       $config = $test->getConfig();
       if (empty($config['import'])) {
         continue;
       }
+
       try {
         $import = $suite->getRunner()->resolveFile($config['import']);
       }
@@ -29,16 +29,9 @@ final class Import extends LegacyPlugin {
         $underscored = $info['dirname'] . '/_' . $info['basename'];
         $import = $suite->getRunner()->resolveFile($underscored);
       }
-      $insert_code = Yaml::parseFile($import);
-      array_splice($new_data, $index, 1, $insert_code);
-      $needs_update = TRUE;
-    }
 
-    if ($needs_update) {
-      $suite->removeAllTests();
-      foreach (array_values($new_data) as $test_index => $config) {
-        $suite->addTest($test_index, $config);
-      }
+      $insert_code = Yaml::parseFile($import);
+      $suite->replaceTestWithMultiple($test, $insert_code);
     }
   }
 
