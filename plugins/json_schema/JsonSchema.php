598db1f3cc2014f852d89cbbbfbd0c4e3a93e62d
Aaron Klump <sourcecode@intheloftstudios.com>
2022-04-15 21:16:29 -0700

    Rewrite json schema plugin to allow for schema interpolation.

--- plugins/json_schema/JsonSchema.php
+++ plugins/json_schema/JsonSchema.php
@@ -20,53 +20,52 @@ final class JsonSchema extends LegacyPlugin {
   const SEARCH_TYPE = 'schema';
 
   /**
-   * Captures the test config to share across methods.
+   * Allows instance caching of decoded schemas.
    *
    * @var array
    */
   private $jsonSchemas;
 
-  /**
-   * {@inheritdoc}
-   */
-  public function onAssertToString(string $stringified, Assert $assert): string {
-    $matches = $assert->matches ?? TRUE;
-    $path = 'Response body';
-    if ($assert->path ?? NULL) {
-      $path = "\"$assert->path\"";
-    }
-
-    return sprintf('%s %s JSON schema: %s', $path, $matches ? 'matches' : 'does not match', $assert->schema);
-  }
 
   /**
-   * {@inheritdoc}
+   * (Load) and decode a schema.
+   *
+   * @param $schema_config_value
+   *   A JSON schema string or a resolvable path to a JSON schema file.
+   *
+   * @return object
    */
-  public function onBeforeDriver(TestEventInterface $event) {
-    $config = $event->getTest()->getConfig();
-    if (!is_array($config) || empty($config['find'])) {
-      return;
-    }
-    foreach ($config['find'] as $assert_id => $assertion) {
-
-      // Resolve paths and load all our JSON schemas.
-      if (is_array($assertion) && array_key_exists(self::SEARCH_TYPE, $assertion)) {
-        $is_json = substr(ltrim($assertion['schema']), 0, 1) === '{';
-        if ($is_json) {
-          $this->jsonSchemas[$assert_id] = $assertion['schema'];
+  private function decodeSchemaValue(string $schema_config_value) {
+    $cid = crc32($schema_config_value);
+    if (!isset($this->jsonSchemas[$cid])) {
+      $is_json = substr(ltrim($schema_config_value), 0, 1) === '{';
+      if ($is_json) {
+        $json = $schema_config_value;
+      }
+      else {
+        try {
+          $path_to_schema = $this->runner->resolveFile($schema_config_value);
         }
-        else {
-          try {
-            $path_to_schema = $this->runner->resolveFile($assertion['schema']);
-          }
-          catch (UnresolvablePathException $exception) {
-            $message = sprintf('Cannot resolve to schema file: "%s".', $assertion['schema']);
-            throw new UnresolvablePathException($assertion['schema'], $message);
-          }
-          $this->jsonSchemas[$assert_id] = file_get_contents($path_to_schema);
+        catch (UnresolvablePathException $exception) {
+          $message = sprintf('Cannot resolve to schema file: "%s".', $schema_config_value);
+          throw new UnresolvablePathException($schema_config_value, $message);
+        }
+        $json = file_get_contents($path_to_schema);
+      }
+
+      try {
+        $schema = json_decode($json, NULL, 512, JSON_THROW_ON_ERROR);
+        if (!is_object($schema)) {
+          throw new \RuntimeException();
         }
       }
+      catch (\Exception $exception) {
+        throw new \RuntimeException(sprintf('Cannot decode JSON schema from "%s".', $schema_config_value));
+      }
+      $this->jsonSchemas[$cid] = $schema;
     }
+
+    return $this->jsonSchemas[$cid];
   }
 
   /**
@@ -91,12 +90,8 @@ final class JsonSchema extends LegacyPlugin {
     $assert->setSearch(self::SEARCH_TYPE);
 
     $assert->setAssertion(Assert::ASSERT_CALLABLE, function ($assert) use ($response) {
-      $assert_id = $assert->getId();
+      $schema = $this->decodeSchemaValue($assert->schema);
       $expected = $assert->matches ?? TRUE;
-      if (empty($this->jsonSchemas[$assert_id])) {
-        throw new \RuntimeException(sprintf('Missing schema for assert %d', $assert_id));
-      }
-      $schema = json_decode($this->jsonSchemas[$assert_id]);
       foreach ($assert->getHaystack() as $data) {
         $validator = new Validator();
         $validator->validate($data, $schema);
@@ -111,6 +106,20 @@ final class JsonSchema extends LegacyPlugin {
 
       return TRUE;
     });
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onAssertToString(string $stringified, Assert $assert): string {
+    $matches = $assert->matches ?? TRUE;
+    $path = 'Response body';
+    if ($assert->path ?? NULL) {
+      $path = "\"$assert->path\"";
+    }
+
+    return sprintf('%s %s JSON schema: %s', $path, $matches ? 'matches' : 'does not match', $assert->schema);
   }
 
 }
