6a648ede0f1eec323f55188ce0a5e891044230a7
Aaron Klump <sourcecode@intheloftstudios.com>
2022-04-15 22:36:17 -0700

    Update schema plugin to new format.

--- plugins/json_schema/JsonSchema.php
+++ plugins/json_schema/JsonSchema.php
@@ -2,18 +2,19 @@
 
 namespace AKlump\CheckPages\Plugin;
 
+use AKlump\CheckPages\Event;
 use AKlump\CheckPages\Event\AssertEventInterface;
-use AKlump\CheckPages\Event\TestEventInterface;
 use AKlump\CheckPages\Assert;
 use AKlump\CheckPages\Exceptions\UnresolvablePathException;
 use AKlump\CheckPages\SerializationTrait;
 use JsonSchema\Validator;
 use MidwestE\ObjectPath;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Implements the Json Schema plugin.
  */
-final class JsonSchema extends LegacyPlugin {
+final class JsonSchema implements EventSubscriberInterface {
 
   use SerializationTrait;
 
@@ -26,55 +27,32 @@ final class JsonSchema extends LegacyPlugin {
    */
   private $jsonSchemas;
 
-
   /**
-   * (Load) and decode a schema.
-   *
-   * @param $schema_config_value
-   *   A JSON schema string or a resolvable path to a JSON schema file.
-   *
-   * @return object
+   * {@inheritdoc}
    */
-  private function decodeSchemaValue(string $schema_config_value) {
-    $cid = crc32($schema_config_value);
-    if (!isset($this->jsonSchemas[$cid])) {
-      $is_json = substr(ltrim($schema_config_value), 0, 1) === '{';
-      if ($is_json) {
-        $json = $schema_config_value;
-      }
-      else {
-        try {
-          $path_to_schema = $this->runner->resolveFile($schema_config_value);
-        }
-        catch (UnresolvablePathException $exception) {
-          $message = sprintf('Cannot resolve to schema file: "%s".', $schema_config_value);
-          throw new UnresolvablePathException($schema_config_value, $message);
-        }
-        $json = file_get_contents($path_to_schema);
-      }
+  public static function getSubscribedEvents() {
+    return [
+      Event::ASSERT_CREATED => [
+        function (AssertEventInterface $event) {
+          $should_apply = boolval($event->getAssert()->schema);
+          if ($should_apply) {
+            $obj = new self();
 
-      try {
-        $schema = json_decode($json, NULL, 512, JSON_THROW_ON_ERROR);
-        if (!is_object($schema)) {
-          throw new \RuntimeException();
-        }
-      }
-      catch (\Exception $exception) {
-        throw new \RuntimeException(sprintf('Cannot decode JSON schema from "%s".', $schema_config_value));
-      }
-      $this->jsonSchemas[$cid] = $schema;
-    }
-
-    return $this->jsonSchemas[$cid];
+            return $obj->prepareAssertion($event);
+          }
+        },
+      ],
+    ];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function onBeforeAssert(AssertEventInterface $event) {
+  public function prepareAssertion(AssertEventInterface $event) {
+    $this->runner = $event->getTest()->getRunner();
     $assert = $event->getAssert();
+    $assert->setToStringOverride([$this, 'onAssertToString']);
     $response = $event->getDriver()->getResponse();
-
     $content_type = $this->getContentType($response);
     $haystack = array_map(function ($item) use ($assert, $content_type) {
       $item = $this->deserialize($item, $content_type);
@@ -108,6 +86,47 @@ final class JsonSchema extends LegacyPlugin {
     });
   }
 
+
+  /**
+   * (Load) and decode a schema.
+   *
+   * @param $schema_config_value
+   *   A JSON schema string or a resolvable path to a JSON schema file.
+   *
+   * @return object
+   */
+  private function decodeSchemaValue(string $schema_config_value) {
+    $cid = crc32($schema_config_value);
+    if (!isset($this->jsonSchemas[$cid])) {
+      $is_json = substr(ltrim($schema_config_value), 0, 1) === '{';
+      if ($is_json) {
+        $json = $schema_config_value;
+      }
+      else {
+        try {
+          $path_to_schema = $this->runner->resolveFile($schema_config_value);
+        }
+        catch (UnresolvablePathException $exception) {
+          $message = sprintf('Cannot resolve to schema file: "%s".', $schema_config_value);
+          throw new UnresolvablePathException($schema_config_value, $message);
+        }
+        $json = file_get_contents($path_to_schema);
+      }
+
+      try {
+        $schema = json_decode($json);
+        if (!is_object($schema)) {
+          throw new \RuntimeException();
+        }
+      }
+      catch (\Exception $exception) {
+        throw new \RuntimeException(sprintf('Cannot decode JSON schema from "%s".', $schema_config_value));
+      }
+      $this->jsonSchemas[$cid] = $schema;
+    }
+
+    return $this->jsonSchemas[$cid];
+  }
 
   /**
    * {@inheritdoc}
