dcee826edb5accfbd7d6b74a402c3defc4a1caac
Aaron Klump <sourcecode@intheloftstudios.com>
2022-11-11 17:10:04 -0800

    build

--- example/web/plugins/thank_you.php
+++ example/web/plugins/thank_you.php
@@ -1,5 +1,7 @@
 <html>
 <body>
 <h1>Thank you <?= $_POST['first_name'] ?>, for your submission</h1>
+<h2>Your membership will begin on <?= $_POST['date'] ?>.</h2>
+<p>Your shirt size is <?= $_POST['shirt_size'] ?>.</p>
 </body>
 </html>
