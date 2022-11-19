9b0d6a71ad14f1bb233c307e5f3019fdff04fa5a
Aaron Klump <sourcecode@intheloftstudios.com>
2022-11-18 17:03:12 -0800

    Remove adamnicholson/bag dependency which no longer supports php 7.

--- src/Storage.php
+++ src/Storage.php
@@ -7,7 +7,7 @@ use Adam\Bag\Bag;
 /**
  * Storage of runner data.
  */
-class Storage extends Bag implements StorageInterface {
+class Storage implements StorageInterface {
 
   /**
    * Path to the file for persistent storage.
@@ -15,6 +15,8 @@ class Storage extends Bag implements StorageInterface {
    * @var string
    */
   protected $diskStorage;
+
+  private $dataset = [];
 
   /**
    * Storage constructor.
@@ -24,29 +26,41 @@ class Storage extends Bag implements StorageInterface {
    *   saved.  If it's directory does not exist then the data will not be saved.
    */
   public function __construct($path_to_disk_storage) {
-    $attributes = [];
     if (!empty($path_to_disk_storage)) {
       $this->diskStorage = dirname($path_to_disk_storage) . '/' . pathinfo($path_to_disk_storage, PATHINFO_FILENAME) . '.json';
       if (file_exists($this->diskStorage)) {
-        $attributes = json_decode(file_get_contents($this->diskStorage), TRUE);
-        if (!is_array($attributes)) {
-          $attributes = [];
+        $this->dataset = json_decode(file_get_contents($this->diskStorage), TRUE);
+        if (!is_array($this->dataset)) {
+          $this->dataset = [];
         }
       }
     }
-    parent::__construct($attributes);
   }
 
   /**
    * Store contents to disk if storage location is known.
    */
   public function __destruct() {
-    if (!empty($this->diskStorage) && $this->all() && is_dir(dirname($this->diskStorage))) {
-      $data = json_encode($this);
+    if (!empty($this->diskStorage) && is_dir(dirname($this->diskStorage))) {
+      $data = json_encode($this->dataset);
       if (FALSE !== $data) {
         file_put_contents($this->diskStorage, $data);
       }
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get($key, $default = NULL) {
+    return $this->dataset[$key] ?? $default;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function set($key, $value) {
+    $this->dataset[$key] = $value;
   }
 
 }
