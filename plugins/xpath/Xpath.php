c2dc8baf4bcfa51bb09debc4f379e5341654be85
Aaron Klump <sourcecode@intheloftstudios.com>
2023-03-12 11:38:52 -0700

    Update xpath plugin.

--- plugins/xpath/Xpath.php
+++ plugins/xpath/Xpath.php
@@ -4,18 +4,13 @@ namespace AKlump\CheckPages\Plugin;
 
 use AKlump\CheckPages\Event;
 use AKlump\CheckPages\Event\AssertEventInterface;
+use AKlump\CheckPages\Exceptions\TestFailedException;
 use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Implements the Xpath plugin.
  */
-final class Xpath implements EventSubscriberInterface {
-
-  /**
-   * @var string
-   */
-  const SEARCH_TYPE = 'xpath';
+final class Xpath implements PluginInterface {
 
   /**
    * {@inheritdoc}
@@ -24,22 +19,39 @@ final class Xpath implements EventSubscriberInterface {
     return [
       Event::ASSERT_CREATED => [
         function (AssertEventInterface $event) {
-          $config = $event->getAssert()->getConfig();
-          $should_apply = array_key_exists(self::SEARCH_TYPE, $config);
+          $should_apply = array_key_exists('xpath', $event->getAssert()
+            ->getConfig());
           if ($should_apply) {
-            $assert = $event->getAssert();
-            $search_value = $assert->{self::SEARCH_TYPE};
-            $assert->setSearch(self::SEARCH_TYPE, $search_value);
-            $haystack = $assert->getHaystack();
-            if (!$haystack instanceof Crawler) {
-              $haystack = new Crawler($haystack);
+            try {
+              // Here will will create an instance and call the method because the
+              // implementation is going to be more complicated and involve
+              // multiple methods.
+              $xpath = new self();
+              $xpath->setHaystack($event);
             }
-            $haystack = $haystack->filterXPath($search_value);
-            $assert->setHaystack($haystack);
+            catch (\Exception $e) {
+              throw new TestFailedException($event->getTest()
+                ->getConfig(), $e);
+            }
           }
         },
       ],
     ];
+  }
+
+  protected function setHaystack(AssertEventInterface $event) {
+    $assert = $event->getAssert();
+    $assert->setSearch('xpath', $assert->xpath);
+    $haystack = $assert->getHaystack();
+    if (!$haystack instanceof Crawler) {
+      $haystack = new Crawler($haystack);
+    }
+    $haystack = $haystack->filterXPath($assert->xpath);
+    $assert->setHaystack($haystack);
+  }
+
+  public static function getPluginId(): string {
+    return 'xpath';
   }
 
 }
