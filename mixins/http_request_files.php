da72ca7a1daca209d7908557e1d7a900487b0ceb
Aaron Klump <sourcecode@intheloftstudios.com>
2023-04-30 18:19:41 -0700

    Fix bug with empty files from http_test_files mixin.

--- mixins/http_request_files.php
+++ mixins/http_request_files.php
@@ -16,8 +16,7 @@
 namespace AKlump\CheckPages\Mixins\HttpRequestFiles;
 
 use AKlump\CheckPages\Event;
-use AKlump\CheckPages\Event\SuiteEventInterface;
-use AKlump\CheckPages\Event\TestEventInterface;
+use AKlump\CheckPages\Event\DriverEventInterface;
 
 /** @var \AKlump\CheckPages\Parts\Runner $runner */
 /** @var array $mixin_config */
@@ -27,32 +26,8 @@ if (!$log_files) {
   throw new \RuntimeException('This mixin must come AFTER load_config(); edit your runner file and move this invocation.');
 }
 
-$mixin = new HttpRequestFiles($log_files, $mixin_config);
-
-respond_to(Event::SUITE_STARTED, function (SuiteEventInterface $event) use ($mixin) {
-  fclose(fopen($mixin->getFilepath($event->getSuite()), 'w'));
-});
-
-respond_to(Event::REQUEST_PREPARED, function (TestEventInterface $event) use ($mixin, $mixin_config) {
-  $http = fopen($mixin->getFilepath($event->getTest()->getSuite()), 'a');
-  try {
-    $export = $mixin->export(
-      $event->getTest(),
-      $event->getDriver()
-    );
-    if (!empty($export)) {
-      fwrite($http, $export);
-    }
-    fclose($http);
-  }
-  catch (\Exception $exception) {
-    fclose($http);
-  }
-});
-
-respond_to(Event::SUITE_FINISHED, function (Event\SuiteEventInterface $event) use ($mixin) {
-  $path = $mixin->getFilepath($event->getSuite());
-  if (file_exists($path) && filesize($path) === 0) {
-    unlink($path);
-  }
+$mixin = new HttpRequestFiles($mixin_config, $log_files);
+respond_to(Event::REQUEST_PREPARED, function (DriverEventInterface $event) use ($mixin) {
+  $file_contents = $mixin->getRequestLogEntry($event->getTest(), $event->getDriver());
+  $mixin->saveRequestToLogFile($file_contents, $event->getTest()->getSuite());
 });
--- src/Mixins/HttpRequestFiles/HttpRequestFiles.php
+++ src/Mixins/HttpRequestFiles/HttpRequestFiles.php
@@ -16,12 +16,12 @@ class HttpRequestFiles {
    */
   private $logFiles;
 
-  public function __construct(LocalFilesProvider $log_files, array $mixin_config) {
+  public function __construct(array $mixin_config, LocalFilesProvider $log_files) {
     $this->options = $mixin_config;
     $this->logFiles = $log_files;
   }
 
-  public function getFilepath(Suite $suite): string {
+  private function getFilepath(Suite $suite): string {
     if ($this->options['single_file'] ?? FALSE) {
       $basename = parse_url($suite->getRunner()->get('base_url'), PHP_URL_HOST);
     }
@@ -34,10 +34,42 @@ class HttpRequestFiles {
     return $filepath;
   }
 
-  public function export(Test $test, RequestDriverInterface $driver): string {
+  /**
+   * Get the log entry contents for a request.
+   *
+   * @param \AKlump\CheckPages\Parts\Test $test
+   * @param \AKlump\CheckPages\Browser\RequestDriverInterface $driver
+   *
+   * @return string
+   */
+  public function getRequestLogEntry(Test $test, RequestDriverInterface $driver): string {
     $why = $test->getConfig()['why'] ?? '';
 
     return HttpLogging::request($why, $driver->getMethod(), (string) $driver->getUri(), $driver->getHeaders(), trim($driver->getBody()));
+  }
+
+  /**
+   * Save the request log entry to file.
+   *
+   * @param string $file_contents
+   * @param \AKlump\CheckPages\Parts\Suite $suite
+   *
+   * @return void
+   */
+  public function saveRequestToLogFile(string $file_contents, Suite $suite) {
+    // The first time we try to write a request for a given suite we're going to
+    // empty truncate the file.  After that we will append.
+    static $suites_started = [];
+    $mode = 'a';
+    if (empty($suites_started[$suite->id()])) {
+      $suites_started[$suite->id()] = TRUE;
+      $mode = 'w';
+    }
+    if (!empty($file_contents)) {
+      $http = fopen($this->getFilepath($suite), $mode);
+      fwrite($http, $file_contents);
+      fclose($http);
+    }
   }
 
 }
