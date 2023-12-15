8ca487d119fee07fe1e2e3b6057e8acc834565f5
Aaron Klump <sourcecode@intheloftstudios.com>
2023-12-14 21:14:40 -0800

    Add set to bash.

--- includes/handlers/bash/Bash.php
+++ includes/handlers/bash/Bash.php
@@ -2,6 +2,7 @@
 
 namespace AKlump\CheckPages\Handlers;
 
+use AKlump\CheckPages\Assert;
 use AKlump\CheckPages\Event;
 use AKlump\CheckPages\Event\TestEventInterface;
 use AKlump\CheckPages\Output\Message;
@@ -12,6 +13,8 @@ use AKlump\Messaging\MessageType;
  * Implements the Bash handler.
  */
 final class Bash implements HandlerInterface {
+
+  use \AKlump\CheckPages\Traits\SetTrait;
 
   /**
    * {@inheritdoc}
@@ -37,7 +40,8 @@ final class Bash implements HandlerInterface {
 
           $bash_output = [];
           $test_result = 0;
-          exec($bash_command, $bash_output, $test_result);
+          $set_value = exec($bash_command, $bash_output, $test_result);
+          $set_key = $test->get('set');
 
           $test_result == 0 ? $test->setPassed() : $test->setFailed();
 
@@ -53,6 +57,29 @@ final class Bash implements HandlerInterface {
             $verbosity
           ));
 
+          if ($set_key) {
+            $config = $test->getConfig();
+            $config['value'] = $set_value;
+            $test->setConfig($config);
+            $handler = new self();
+            $lines[] = $handler->setKeyValuePair(
+              $test->getSuite()->variables(),
+              $config[Assert::ASSERT_SETTER],
+              $config['value']
+            );
+            $test->addMessage(new Message($lines, MessageType::DEBUG, Verbosity::DEBUG));
+          }
+        },
+      ],
+      Event::TEST_FINISHED => [
+        function (TestEventInterface $event) {
+          $test = $event->getTest();
+          if ($test->has(Assert::ASSERT_SETTER)) {
+            $suite = $test->getSuite();
+            $config = $test->getConfig();
+            $suite->interpolate($config['value']);
+            $suite->interpolate($config[Assert::ASSERT_SETTER]);
+          }
         },
       ],
     ];
