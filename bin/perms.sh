4d26fa864b6a9da3efa3f5bda407a2e6b5e87cc0
Aaron Klump <sourcecode@intheloftstudios.com>
2023-12-15 12:44:05 -0800

    Add perms script.

---
+++ bin/perms.sh
@@ -0,0 +1,6 @@
+s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$d/$s";done;__DIR__=$(cd -P "$(dirname "$s")" && pwd)
+
+cd $__DIR__/
+chmod u+x *.sh
+chmod u+x *.php
+chmod u+x $__DIR__/../vendor/bin/phpunit
