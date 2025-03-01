acba3281c64fdef920bec4ef7ed014751a48248a
Aaron Klump <sourcecode@intheloftstudios.com>
2025-03-01 09:07:35 -0800

    Add session management mixin for user-authenticated tests
    Introduce a mixin to handle user sessions in tests via configuration files. This enables specifying user records and session cookies, seamlessly integrating session management into test requests. Documentation and setup examples are included for easy implementation.

---
+++ mixins/sessions.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AKlump\CheckPages\Mixins\Session;
+
+use AKlump\CheckPages\Browser\Session;
+use AKlump\CheckPages\Event;
+use AKlump\CheckPages\Event\DriverEventInterface;
+use AKlump\CheckPages\Event\SuiteEventInterface;
+use AKlump\CheckPages\Event\TestEventInterface;
+use AKlump\CheckPages\Exceptions\StopRunnerException;
+use AKlump\CheckPages\Files\LoadUsers;
+use AKlump\CheckPages\Helpers\FilterUsersByName;
+use AKlump\LoftLib\Bash\Color;
+
+/**
+ * Provide Check Pages with a Mixin Called My Mixin
+ *
+ * 1. Create config/users.yml or config/users.json.
+ * 1. Add at least one user record as shown below.
+ * 1. Register this mixin in your runner file.
+ * 1. Add the `user` key to any test and the session cookie will be sent in
+ * the request for that test.s
+ *
+ * Usage in config/users.yml:
+ *
+ * @code
+ * -
+ *   user: foobar
+ *   session: SSESSf99b4ea981b1850593a7c4b0249ea3a2=0YxPBfvlZGApBk6d9YBdMdvXs0Yjg5WaFBmFMMZ4VEY4ponu
+ * @endcode
+ *
+ * Usage in runner.php:
+ *
+ * @code
+ * add_mixin('mixins/session', [
+ *   'users' => 'config/users.yml',
+ * ]);
+ * @endcode
+ *
+ * Usage in your test file:
+ *
+ * @code
+ * -
+ *   visit: /
+ *   user: foobar
+ * @endcode
+ */
+
+/** @var \AKlump\CheckPages\Files\FilesProviderInterface $files */
+/** @var \AKlump\CheckPages\Parts\Runner $runner */
+/** @var array $mixin_config */
+
+$credentials_file = $mixin_config['users'] ?? '';
+$credentials_file = $files->tryResolveFile($credentials_file)[0] ?? '';
+$users = (new LoadUsers())($credentials_file);
+
+add_test_option('user', [
+  Event::REQUEST_CREATED => function ($username, DriverEventInterface $event) use ($users) {
+    if (empty($username)) {
+      return;
+    }
+
+    //
+    // Locate the session by username
+    //
+    $session = new Session();
+    $user = (new FilterUsersByName($users))($username);
+    if ($user) {
+      $session->setUser($user);
+    }
+    $session->setSessionCookie($user ? $user->getProperty('session') : '');
+
+    //
+    // Set the session cookie header
+    //
+    $cookie = $session->getSessionCookie();
+    if (!$cookie) {
+      throw new StopRunnerException("No session found for user $username");
+    }
+    $event->getDriver()->setHeader('Cookie', $cookie);
+
+    //
+    // Add visual feedback that the request is authenticated.
+    //
+    $output = $event->getTest()->getRunner()->getOutput();
+    if ($output->isVeryVerbose()) {
+      $event->getTest()
+        ->addBadge(sprintf('👤%s ', Color::wrap('light gray', $session->getUser()
+          ->getAccountName())));
+    }
+    else {
+      $event->getTest()->addBadge('👤');
+    }
+  },
+]);
