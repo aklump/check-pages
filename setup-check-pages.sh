dab7e3f09518c2cf146dfa5dff6f42a119642406
Aaron Klump <sourcecode@intheloftstudios.com>
2025-03-05 15:01:49 -0800

    Build

--- setup-check-pages.sh
+++ setup-check-pages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ##
- # Install Check Pages as a Stand Along composer project
+ # Install Check Pages for a single user.
  #
  # @param string Optional, Check Pages version constraint e.g. "0.19.1", "@dev"
  #
@@ -40,7 +40,7 @@ function check_composer() {
 status "Directory \"$install_path\" created."
 
 # Composer installation
-! printf '{"name":"aklump/check-pages-project","type":"project","require":{"aklump/check-pages":"%s"},"config":{"allow-plugins":{"wikimedia/composer-merge-plugin":true}}}' "$install_version" > composer.json && echo '' && exit 1
+! printf '{"name":"aklump/check-pages-project","type":"project","require":{"aklump/check-pages":"%s"}}' "$install_version" > composer.json && echo '' && exit 1
 ! composer install && error 'Cannot install dependencies.' && exit 1
 
 command_path='bin/checkpages'
@@ -53,5 +53,5 @@ suggest 'Optional: To make checkpages accessible from anywhere, add the followin
 suggest 'to your shell startup file (e.g., ~/.bashrc, ~/.zshrc, or equivalent):'
 echo "export PATH=\"${install_path/$HOME/\$HOME}/$(dirname "$command_path"):\$PATH\""
 suggest 'After adding the line, either restart your terminal or source the updated file.'
-status "Thank you for installing Check Pages, happy testing!"
+status "Thank you for installing Check Pages!"
 status "Get help: $install_path/$command_path"
