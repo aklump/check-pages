54d776a44731f225ebef1654abb5bc1b2c8cb5af
Aaron Klump <sourcecode@intheloftstudios.com>
2025-05-10 15:04:27 -0700

    Add note to start testing server for Chrome Test.

--- tests_phpunit/Unit/Browser/ChromeDriverTest.php
+++ tests_phpunit/Unit/Browser/ChromeDriverTest.php
@@ -4,6 +4,7 @@ namespace AKlump\CheckPages\Tests\Unit\Browser;
 
 use AKlump\CheckPages\Browser\ChromeDriver;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 /**
  * @covers \AKlump\CheckPages\Browser\ChromeDriver
@@ -13,12 +14,19 @@ class ChromeDriverTest extends TestCase {
   public function testChromeDriverReturnsCorrectRedirectAndStatusCodes() {
     $driver = new ChromeDriver();
     $driver->setUrl('http://localhost:8000/moved.php');
-    $response = $driver->request();
+    try {
+      $response = $driver->request();
+      $redirect_code = $response->getRedirectCode();
+      $this->assertSame(301, $redirect_code);
 
-    $redirect_code = $response->getRedirectCode();
-    $this->assertSame(301, $redirect_code);
-
-    $final_status = $response->getResponse()->getStatusCode();
-    $this->assertSame(200, $final_status);
+      $final_status = $response->getResponse()->getStatusCode();
+      $this->assertSame(200, $final_status);
+    }
+    catch (RuntimeException $exception) {
+      if (str_contains($exception->getMessage(), 'Connection refused')) {
+        throw new RuntimeException('Start the test server: bin/start_test_server.sh');
+      }
+      throw $exception;
+    }
   }
 }
